78fa0a3604a5ca087e47c6b37fc09f7fd31ad478
ananduvkk78 <anandakrishnan.v@thinkpalm.com>
2018-10-16 09:46:24 +0530

    committed usertests and admintests

---
+++ test/adminServiceTests.js
@@ -0,0 +1,100 @@
+const sinon = require("sinon");
+const admin = require("../src/services/adminServices");
+const assert = require("chai").assert;
+const adminobj = new admin();
+
+
+
+/**
+ * Testing the admin method of adminServices
+ */
+describe("checking the admin method", () => {
+    let results = {};
+    let adminstub;
+    before(() => {
+        results = {
+            rows: [{
+                id: 140,
+                name: "balu",
+                address: "djsojiopp",
+                username: "balu",
+                email: "abhishekmsam54@gmail.com",
+                password: "$2b$10$9PMA8rzwN1s3mISzF0KM9OJOUmovUF7gwwQcKVqQdfV5cnCczjKaC",
+                phone_no: "0987654321",
+                role: "u"
+            }]
+        };
+
+        adminstub = sinon.stub(adminobj, "execute");
+        adminstub.resolves(results);
+    });
+    it("should  execute and return all user details", () => {
+        return adminobj.admin().then((res) => {
+
+            assert.equal(res, results.rows);
+        }).catch((err) => {
+
+            assert.equal(err, false);
+        });
+    });
+    afterEach(() => {
+        adminstub.restore();
+    });
+});
+
+
+/** testing change function */
+describe("checking the change method", () => {
+    let id = 155;
+    let response = "Admin privilege";
+    let selectstub;
+    before(() => {
+        let results = {
+            rows: [{
+                count: 1
+            }]
+        };
+        selectstub = sinon.stub(adminobj, "select");
+        selectstub.resolves(results);
+    });
+    it("should  execute and return all user details", () => {
+        return adminobj.change(id).then((res) => {
+            assert.equal(res, response);
+        }).catch((err) => {
+            assert.equal(err, false);
+        });
+    });
+    afterEach(() => {
+        selectstub.restore();
+    });
+});
+
+/** testing delete function */
+
+describe("checking the delete method", () => {
+    let id = 159;
+    let response = "Account Deleted";
+    let selectstub;
+    before(() => {
+        let results = {
+            rows: [{
+                count: 1
+            }]
+
+        };
+        selectstub = sinon.stub(adminobj, "select");
+        selectstub.resolves(results);
+    });
+    it("should  execute and return response message", () => {
+        return adminobj.delete(id).then((res) => {
+            assert.equal(res, response);
+        }).catch((err) => {
+
+            assert.equal(err, false);
+
+        });
+    });
+    afterEach(() => {
+        selectstub.restore();
+    });
+});
---
+++ test/userServiceTests.js
@@ -0,0 +1,171 @@
+const sinon = require("sinon");
+const user = require("../src/services/userServices");
+const assert = require("chai").assert;;
+const userobj = new user();
+
+/**
+ * Testing the home method of userservices
+ */
+describe("checking the home method", () => {
+    let results = {};
+    let homestub;
+    before(() => {
+        results = {
+            rows: [{
+                id: 140,
+                name: "balu",
+                address: "djsojiopp",
+                username: "balu",
+                email: "abhishekmsam54@gmail.com",
+                password: "$2b$10$9PMA8rzwN1s3mISzF0KM9OJOUmovUF7gwwQcKVqQdfV5cnCczjKaC",
+                phone_no: "0987654321",
+                role: "u"
+            }]
+        };
+
+        homestub = sinon.stub(userobj, "execute");
+        homestub.resolves(results);
+    });
+    it("should  execute and return user details", () => {
+        return userobj.home().then((res) => {
+
+            assert.equal(res, results);
+        }).catch((err) => {
+
+            assert.equal(err, false);
+
+        });
+    });
+    afterEach(() => {
+        homestub.restore();
+    });
+});
+
+/**
+ * testing login function of userservices
+ */
+describe("checking the login method", () => {
+    let ob = {
+        username: "balu",
+        password: "balu@1234"
+    };
+    let results = {};
+    let loginstub;
+    let status = { "id": 140, "stat": 1, "role": "u" };
+    before(() => {
+        results = {
+            rows: [{
+                id: 140,
+                name: "balu",
+                address: "djsojiopp",
+                username: "balu",
+                email: "abhishekmsam54@gmail.com",
+                password: "$2b$10$9PMA8rzwN1s3mISzF0KM9OJOUmovUF7gwwQcKVqQdfV5cnCczjKaC",
+                phone_no: "0987654321",
+                role: "u"
+            }]
+        };
+
+        loginstub = sinon.stub(userobj, "execute");
+        loginstub.resolves(results);
+    });
+    it("should  execute and return a status object ", () => {
+        return userobj.login(ob).then((res) => {
+            assert.equal(res.stat, status.stat);
+        }).catch((err) => {
+            assert.equal(err, false);
+
+        });
+    });
+    afterEach(() => {
+        loginstub.restore();
+    });
+});
+
+/**
+ * testing the register method
+ */
+describe("checking the register method", () => {
+    let ob = {
+        name: "balu",
+        addr: "djsojiopp",
+        usr: "kasp",
+        mail: "abhishekmsam54@gmail.com",
+        pwd: "balu@1234",
+        no: "0987654321",
+        role: "u"
+    };
+    let results = {};
+    let selectstub;
+    let insertstub;
+    let status = {
+        rows: [{ id: 123, stat: 1 }]
+    };
+
+
+    before(() => {
+        results = {
+            rows: [{
+
+                count: 0
+            }]
+        };
+        selectstub = sinon.stub(userobj, "countRegister");
+        selectstub.resolves(results);
+        insertstub = sinon.stub(userobj, "getid");
+        insertstub.resolves(status);
+
+    });
+    it("should  execute and return   status", () => {
+        return userobj.register(ob).then((res) => {
+            assert.equal(res.stat, status.rows[0].stat);
+        }).catch((err) => {
+            assert.equal(err, false);
+
+        });
+    });
+    afterEach(() => {
+        selectstub.restore();
+
+    });
+});
+
+/**  testing the edit function */
+
+describe("checking the edit method", () => {
+    let ob = {
+        name: "balu",
+        add: "djsojiopp",
+        usr: "kasp",
+        mail: "abhishekmsam54@gmail.com",
+        pass: "balu@1234",
+        phone: "0987654321",
+        role: "u",
+        id: 140
+    };
+    let results = {};
+    let response = "Details updated successfully";
+
+    let updateustub;
+
+    before(() => {
+        results = {
+            rows: [{ count: "0" }]
+        };
+        updateustub = sinon.stub(userobj, "checkEntry");
+        updateustub.resolves(results);
+    });
+    it("should  execute and return   status message", () => {
+        return userobj.edit(ob).then((res) => {
+            assert.equal(res, response);
+        }).catch((err) => {
+
+            assert.equal(err, false);
+
+        });
+    });
+    afterEach(() => {
+        updateustub.restore();
+
+    });
+});
